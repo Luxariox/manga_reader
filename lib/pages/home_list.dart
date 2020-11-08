149dccc67571000188d7cd4bf4a9f9fcb97fd19a
Luxariox <theotime.terrien@epita.fr>
2020-11-08 18:04:34 +0100

    [EDIT] - Can now reload chapter list

--- lib/pages/home_list.dart
+++ lib/pages/home_list.dart
@@ -5,22 +5,43 @@ import 'package:manga_reader/model/mangadex_api.dart';
 import 'package:manga_reader/model/sorter.dart';
 import 'package:manga_reader/widget/chapter_item.dart';
 
-class HomeList extends StatelessWidget {
+class HomeList extends StatefulWidget {
+  @override
+  _HomeListState createState() => _HomeListState();
+}
+
+class _HomeListState extends State<HomeList> {
+  Future<List<ChapterData>> chapterList;
+
+  @override
+  void initState() {
+    chapterList = _fetchChapterList();
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Manga Reader'),
+        actions: [
+          IconButton(icon: Icon(Icons.refresh), onPressed: () {
+            setState(() {
+              chapterList = _fetchChapterList();
+            });
+          }),
+        ],
       ),
       drawer: Drawer(),
       body: FutureBuilder(
-        future: MangadexApi.getChaptersList([21063, 17274]),
+        future: chapterList,
         builder: (context, snapshot) {
           if (snapshot.hasError) {
             print(snapshot.error);
           }
 
-          if (snapshot.hasData) {
+          if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
             var unfilteredData = snapshot.data as List<ChapterData>;
             var data = LanguageFilter.chapterData(unfilteredData, ['gb', 'fr']);
             Sorter.chapterDataTimestamp(data);
@@ -63,5 +84,9 @@ class HomeList extends StatelessWidget {
 
   bool _needMangaTitle(List<ChapterData> data, int i) {
     return i == 0 || data[i - 1].mangaId != data[i].mangaId;
+  }
+
+  Future<List<ChapterData>> _fetchChapterList() {
+    return MangadexApi.getChaptersList([21063, 17274]);
   }
 }
