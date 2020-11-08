b6f360a3b4b2a126c97852a04aac203df28b852f
Luxariox <theotime.terrien@epita.fr>
2020-11-08 13:38:19 +0100

    [EDIT] - ChapterItem shows title and chapter number

--- lib/widget/chapter_item.dart
+++ lib/widget/chapter_item.dart
@@ -6,10 +6,18 @@ class ChapterItem extends StatelessWidget {
 
   ChapterItem({@required this.chapter});
 
+  String _getTitle() {
+    if (chapter.title != null && chapter.title.isNotEmpty) {
+      return 'Ch. ${chapter.chapter} - ${chapter.title}';
+    } else {
+      return 'Ch. ${chapter.chapter}';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(chapter.title),
+      title: Text(_getTitle()),
     );
   }
 }
