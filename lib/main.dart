9f1954042229d1dc224d780bc06206a4b61825e7
Luxariox <theotime.terrien@epita.fr>
2020-11-11 16:52:38 +0100

    [EDIT] - UserData methods

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:manga_reader/model/user_data.dart';
 import 'package:manga_reader/pages/home_list.dart';
 import 'package:manga_reader/routes.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  var userData = UserData();
+
+  await userData.initialize();
+  userData.loadData();
+
+  runApp(MyApp(userData: userData));
 }
 
 class MyApp extends StatelessWidget {
+  final UserData userData;
+
+  MyApp({this.userData});
+
   @override
   Widget build(BuildContext context) {
-    var drawer = Drawer();
-
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    var drawer = Drawer(
+      child: SafeArea(
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('Feed'),
+              onTap: () {},
+            ),
+          ],
+        ),
       ),
-      routes: {
-        Routes.home: (_) => HomeList(drawer: drawer),
-      },
+    );
+
+    return Provider.value(
+      value: userData,
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        routes: {
+          Routes.home: (_) => HomeList(drawer: drawer),
+        },
+      ),
     );
   }
 }
-
-
-/*
-Drawer(
-  child: SafeArea(
-    child: Column(
-      children: [
-        ListTile(
-          title: Text('Feed'),
-          onTap: () {
-            setState(() {
-              page = MainPages.home;
-            });
-          },
-        ),
-      ],
-    ),
-  ),
-);
- */
--- lib/model/user_data.dart
+++ lib/model/user_data.dart
@@ -1,5 +1,46 @@
-class UserData {
-  Map<int, String> titles;
+import 'package:flutter/cupertino.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-  UserData(this.titles);
+class UserData extends ChangeNotifier {
+  Map<int, String> titles;
+  SharedPreferences _sharedPreferences;
+
+  UserData({this.titles});
+
+  Future<void> initialize() async {
+    _sharedPreferences = await SharedPreferences.getInstance();
+  }
+
+  void loadData() {
+    loadTitles();
+  }
+
+  void loadTitles() {
+    var titlesList = _sharedPreferences
+        .getStringList('titles')
+        .map((e) => int.parse(e))
+        .toList();
+
+    for (var title in titlesList) {
+      titles[title] = '$title';
+    }
+  }
+
+  void addTitle(int id, String title) {
+    titles[id] = title;
+
+    var store = titles.keys.map((e) => '$e').toList();
+    _sharedPreferences.setStringList('titles', store);
+
+    notifyListeners();
+  }
+
+  void removeTitle(int id) {
+    titles.remove(id);
+
+    var store = titles.keys.map((e) => '$e').toList();
+    _sharedPreferences.setStringList('titles', store);
+
+    notifyListeners();
+  }
 }
