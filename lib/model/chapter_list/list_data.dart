7b504cb92165e3903cb7f4b1ace376348950261d
Luxariox <theotime.terrien@epita.fr>
2020-11-11 10:39:54 +0100

    [FIX] - Request deprecated

--- lib/model/chapter_list/list_data.dart
+++ lib/model/chapter_list/list_data.dart
@@ -1,17 +1,20 @@
+import 'package:manga_reader/model/chapter/group.dart';
+
 import 'data.dart';
 
 class ChapterListData {
   List<ChapterData> chapters;
-  Map<String, String> groups;
+  List<Group> groups;
 
   ChapterListData({this.chapters, this.groups});
 
   factory ChapterListData.fromJson(Map<String, dynamic> json) {
     var chapterList = json['chapters'] as List;
+    var groupList = json ['groups'] as List;
 
     return ChapterListData(
       chapters: chapterList.map((e) => ChapterData.fromJson(e)).toList(),
-      groups: Map<String, String>.from(json['groups']),
+      groups: groupList.map((e) => Group.fromJson(e)).toList(),
     );
   }
 }
